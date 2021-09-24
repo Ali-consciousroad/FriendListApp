975afd98add4dd741f9cd7d9c1a322a8a184fa91
Ali-consciousroad <ali_dindar@live.be>
2021-09-24 15:53:27 +0200

    Adding validation & resetting logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,40 +1,57 @@
-import React, { useState } from 'react';
-import Button from '../UI/Button';
-import Card from '../UI/Card';
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
 // Extension need to be added for non js file
-import classes from './AddUser.module.css';
+import classes from "./AddUser.module.css";
 
-const AddUser = props => {
-    // Array destructuring
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
-    const addUserHandler = (event) => {
-        event.preventDefault();
-        console.log(enteredUsername, enteredAge);
-    };
-    // Tringered by the input written in the form
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    };
+const AddUser = (props) => {
+  // Array destructuring
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+        return; 
+    }
+    if (+enteredAge < 1){
+        return; 
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+  // Tringered by the input written in the form
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
 
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
 
-
-    return (
-        <Card className={classes.input}>
-            {/*Don't forget, we don't want to execute the fonction here, we only want to execute it*/}
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler} />
-                {/* We can't use " for " with JSX, we need to use htmlFor instead */}
-                <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" onChange={ageChangeHandler} />
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
-    );
+  return (
+    <Card className={classes.input}>
+      {/*Don't forget, we don't want to execute the fonction here, we only want to execute it*/}
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+        {/* We can't use " for " with JSX, we need to use htmlFor instead */}
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
 };
 
-export default AddUser; 
+export default AddUser;
