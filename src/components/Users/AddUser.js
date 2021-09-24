2660bf84101a7c1b5757007c2216841854317010
Ali-consciousroad <ali_dindar@live.be>
2021-09-24 15:22:53 +0200

    Managing the user input state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 // Extension need to be added for non js file
 import classes from './AddUser.module.css';
 
 const AddUser = props => {
+    // Array destructuring
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUsername, enteredAge);
     };
+    // Tringered by the input written in the form
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    };
+
 
     return (
         <Card className={classes.input}>
             {/*Don't forget, we don't want to execute the fonction here, we only want to execute it*/}
             <form onSubmit={addUserHandler}>
-                <label class="">Username</label>
-                <input id="username" type="text" />
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" onChange={usernameChangeHandler} />
                 {/* We can't use " for " with JSX, we need to use htmlFor instead */}
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number"></input>
+                <input id="age" type="number" onChange={ageChangeHandler} />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
